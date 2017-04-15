38767c3dfd45703d76c28b1cdca3b5d4274f3088
Samuel Reed <samuel.trace.reed@gmail.com>
2017-04-15 11:51:26 -0400

    Remove md5 dependency, use webpack options to determine hash options.

---
+++ test/one-asset-sha256/webpack.config.js
@@ -0,0 +1,17 @@
+const OutputHash = require('../../src/OutputHash.js');
+const path = require("path");
+
+const rel = (paths => path.resolve(__dirname, ...paths))
+
+module.exports = {
+    entry: rel`./entry.js`,
+    output: {
+        path: rel`../tmp`,
+        filename: '[name].[chunkhash].js',
+        hashFunction: 'sha1',
+    },
+    plugins: [
+        new OutputHash(),
+    ],
+};
+
--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
+const crypto = require('crypto');
 const webpack = require('webpack');
 const path = require('path');
-const md5 = require('md5');
 const expect = require('chai').expect;
 
-const expetAssetsNameToContainHash = (assets, filter = ()=>true) => {
+const expectAssetsNameToContainHash = (stats, filter = ()=>true) => {
+    const {assets} = stats.compilation;
+    const {hashFn} = stats;
     expect(Object.keys(assets)).to.have.length.at.least(1);
     Object.keys(assets).filter(filter).forEach(name => {
         const asset = assets[name];
-        const contentHash = md5(asset.source()).substr(0, 20);
-        expect(name).to.contain(contentHash);
+        const { shortHash } = hashFn(asset.source());
+        expect(name).to.contain(shortHash);
     });
 }
 
@@ -16,13 +18,29 @@ const webpackCompile = (fixture) => {
     return new Promise((resolve, reject)=> {
         const dir = path.resolve(__dirname, fixture);
         const config = path.resolve(dir, 'webpack.config.js');
-        webpack(Object.assign(require(config), {
-            context: dir,
-        }), (err, stats) => {
+        const opts = Object.assign(require(config), { context: dir });
+        webpack(opts, (err, stats) => {
+            // Just attach the hashFn here while we have the outputOptions so we can assert with it.
+            stats.hashFn = makeHashFn(opts.output);
             if (err) reject(err);
             else resolve(stats);
         });
     })
+}
+
+// Each compilation may use a different hash fn, so we need to generate one from the webpack outputOptions.
+function makeHashFn({
+    hashFunction = 'md5',
+    hashDigest = 'hex',
+    hashDigestLength = 20,
+    hashSalt = null,
+} = {}) {
+    return (input) => {
+        const hashObj = crypto.createHash(hashFunction).update(input);
+        if (hashSalt) hashObj.update(hashSalt);
+        const fullHash = hashObj.digest(hashDigest);
+        return { fullHash, shortHash: fullHash.substr(0, hashDigestLength) };
+    };
 }
 
 const findAssetByName = (assets, name) => {
@@ -40,19 +58,38 @@ const extractHashes = (assets, filter) => {
 describe('OutputHash', () => {
     it("Works with single entry points", () => webpackCompile('one-asset')
         .then((stats) => {
-            expetAssetsNameToContainHash(stats.compilation.assets);
+            expectAssetsNameToContainHash(stats);
+        })
+    )
+
+    // Waiting on https://github.com/webpack/webpack/pull/4717
+    xit("Works with hashSalt", () => webpackCompile('one-asset-salt')
+        .then((stats) => {
+            expectAssetsNameToContainHash(stats);
+        })
+    )
+
+    it("Works with hashFunction (sha256)", () => webpackCompile('one-asset-sha256')
+        .then((stats) => {
+            expectAssetsNameToContainHash(stats);
+        })
+    )
+
+    it("Works with hashDigest (base64)", () => webpackCompile('one-asset-base64')
+        .then((stats) => {
+            expectAssetsNameToContainHash(stats);
         })
     )
 
     it("Works with multiple entry points", () => webpackCompile('multi-asset')
         .then((stats) => {
-            expetAssetsNameToContainHash(stats.compilation.assets);
+            expectAssetsNameToContainHash(stats);
         })
     )
 
     it("Works with common chunks", () => webpackCompile('common-chunks')
         .then((stats) => {
-            expetAssetsNameToContainHash(stats.compilation.assets);
+            expectAssetsNameToContainHash(stats);
 
             const hashes = extractHashes(stats.compilation.assets, n => n[0]!=="vendor")
             const commons = findAssetByName(stats.compilation.assets, "vendor");
@@ -66,7 +103,7 @@ describe('OutputHash', () => {
 
     it("Works with manifest file", () => webpackCompile('manifest')
         .then((stats) => {
-            expetAssetsNameToContainHash(stats.compilation.assets);
+            expectAssetsNameToContainHash(stats);
 
             const hashes = extractHashes(stats.compilation.assets, n => n[0]!=="manifest")
             const commons = findAssetByName(stats.compilation.assets, "manifest");
@@ -80,7 +117,7 @@ describe('OutputHash', () => {
 
     it("Works with HTML output", () => webpackCompile('html')
         .then((stats) => {
-            expetAssetsNameToContainHash(stats.compilation.assets, name => name!=="index.html");
+            expectAssetsNameToContainHash(stats, name => name!=="index.html");
 
             const hashes = extractHashes(stats.compilation.assets, n => n[0]!=="index")
             const commons = findAssetByName(stats.compilation.assets, "index");
