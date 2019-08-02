9fc372a3c4288de9bd749dc93a0b7ab8584edde5
Marcin Szczepanski <mszczepanski@atlassian.com>
2019-08-02 15:24:11 +1000

    Update webpack, add failing tests

--- test/test.js
+++ test/test.js
@@ -53,12 +53,12 @@ const sanityCheck = stats => {
     return stats;
 };
 
-const webpackCompile = (fixture, mode) =>
+const webpackCompile = (fixture, mode, customConfig = config => config) =>
     new Promise((resolve, reject) => {
         const dir = path.resolve(__dirname, fixture);
         const config = path.resolve(dir, 'webpack.config.js');
         // eslint-disable-next-line global-require
-        const opts = Object.assign(require(config), { mode, context: dir });
+        const opts = customConfig(Object.assign(require(config), { mode, context: dir }));
         webpack(opts, (err, stats) => {
             if (err) reject(err);
             else resolve(stats);
@@ -91,98 +91,116 @@ describe('OutputHash', () => {
 
     modes.forEach(mode => {
         context(`In ${mode} mode`, () => {
-            it('Works with single entry points', () =>
-                webpackCompile('one-asset', mode).then(sanityCheck));
+            [true, false].forEach(withFutureEmitAssets => {
+                context(`With futureEmitAssets ${withFutureEmitAssets}`, () => {
+                    const setFutureEmitAssets = config => {
+                        config.output.futureEmitAssets = withFutureEmitAssets;
+                        return config;
+                    };
+                    it('Works with single entry points', () =>
+                        webpackCompile('one-asset', mode, setFutureEmitAssets).then(sanityCheck));
 
-            it('Works with hashSalt', () =>
-                webpackCompile('one-asset-salt', mode).then(sanityCheck));
+                    it('Works with hashSalt', () =>
+                        webpackCompile('one-asset-salt', mode, setFutureEmitAssets).then(
+                            sanityCheck
+                        ));
 
-            it('Works with hashFunction (sha256)', () =>
-                webpackCompile('one-asset-sha256', mode).then(sanityCheck));
+                    it('Works with hashFunction (sha256)', () =>
+                        webpackCompile('one-asset-sha256', mode, setFutureEmitAssets).then(
+                            sanityCheck
+                        ));
 
-            it('Works with hashDigest (base64)', () =>
-                webpackCompile('one-asset-base64', mode).then(sanityCheck));
+                    it('Works with hashDigest (base64)', () =>
+                        webpackCompile('one-asset-base64', mode, setFutureEmitAssets).then(
+                            sanityCheck
+                        ));
 
-            it('Works with multiple entry points', () =>
-                webpackCompile('multi-asset', mode).then(sanityCheck));
+                    it('Works with multiple entry points', () =>
+                        webpackCompile('multi-asset', mode, setFutureEmitAssets).then(sanityCheck));
 
-            it('Works with common chunks', () =>
-                webpackCompile('common-chunk', mode).then(sanityCheck));
+                    it('Works with common chunks', () =>
+                        webpackCompile('common-chunk', mode, setFutureEmitAssets).then(
+                            sanityCheck
+                        ));
 
-            it('Works with async chunks', () =>
-                webpackCompile('async-chunk', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const main = asset(stats, 'main');
-                        const asyncAsset = asset(stats, '0');
-                        expect(main.content).to.contain(asyncAsset.hash);
-                    }));
+                    it('Works with async chunks', () =>
+                        webpackCompile('async-chunk', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const main = asset(stats, 'main');
+                                const asyncAsset = asset(stats, '0');
+                                expect(main.content).to.contain(asyncAsset.hash);
+                            }));
 
-            it('Works with runtime chunks', () =>
-                webpackCompile('runtime-chunk', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const asyncAsset = asset(stats, '0');
-                        const runtime1 = asset(stats, 'runtime~entry1');
-                        const runtime2 = asset(stats, 'runtime~entry2');
+                    it('Works with runtime chunks', () =>
+                        webpackCompile('runtime-chunk', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const asyncAsset = asset(stats, '0');
+                                const runtime1 = asset(stats, 'runtime~entry1');
+                                const runtime2 = asset(stats, 'runtime~entry2');
 
-                        expect(runtime1.content).to.contain(asyncAsset.hash);
-                        expect(runtime2.content).to.contain(asyncAsset.hash);
-                    }));
+                                expect(runtime1.content).to.contain(asyncAsset.hash);
+                                expect(runtime2.content).to.contain(asyncAsset.hash);
+                            }));
 
-            it('Works when runtime has common chunks that require async files', () =>
-                webpackCompile('common-runtime-chunk', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const manifest = asset(stats, 'manifest');
-                        const asyncAsset = asset(stats, '0');
-                        expect(manifest.content).to.contain(asyncAsset.hash);
-                    }));
+                    it('Works when runtime has common chunks that require async files', () =>
+                        webpackCompile('common-runtime-chunk', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const manifest = asset(stats, 'manifest');
+                                const asyncAsset = asset(stats, '0');
+                                expect(manifest.content).to.contain(asyncAsset.hash);
+                            }));
 
-            it('Works when there are two async files requiring each other', () =>
-                webpackCompile('loop', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const entry = asset(stats, 'entry');
-                        const asyncAsset = asset(stats, '0');
-                        expect(entry.content).to.contain(asyncAsset.hash);
-                    }));
+                    it('Works when there are two async files requiring each other', () =>
+                        webpackCompile('loop', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const entry = asset(stats, 'entry');
+                                const asyncAsset = asset(stats, '0');
+                                expect(entry.content).to.contain(asyncAsset.hash);
+                            }));
 
-            it('Works with shared runtime chunk', () =>
-                webpackCompile('shared-runtime-chunk', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const asyncChunk = asset(stats, 'async');
-                        const runtime = asset(stats, 'runtime');
-                        expect(runtime.content).to.contain(asyncChunk.hash);
-                    }));
+                    it('Works with shared runtime chunk', () =>
+                        webpackCompile('shared-runtime-chunk', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const asyncChunk = asset(stats, 'async');
+                                const runtime = asset(stats, 'runtime');
+                                expect(runtime.content).to.contain(asyncChunk.hash);
+                            }));
 
-            it('Works with HTML output', () =>
-                webpackCompile('html', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const index = asset(stats, 'index', '.html');
-                        const entry = asset(stats, 'entry', '.js');
-                        const vendor = asset(stats, 'vendor', '.js');
-                        expect(index.content).to.contain(entry.hash);
-                        expect(index.content).to.contain(vendor.hash);
-                    }));
+                    it('Works with HTML output', () =>
+                        webpackCompile('html', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const index = asset(stats, 'index', '.html');
+                                const entry = asset(stats, 'entry', '.js');
+                                const vendor = asset(stats, 'vendor', '.js');
+                                expect(index.content).to.contain(entry.hash);
+                                expect(index.content).to.contain(vendor.hash);
+                            }));
 
-            it('Works with mini-css-extract-plugin', () =>
-                webpackCompile('mini-css-chunks', mode)
-                    .then(sanityCheck)
-                    .then(stats => {
-                        const runtime = asset(stats, 'runtime~main');
-                        const asyncJs = asset(stats, '0', '.js');
-                        const asyncCss = asset(stats, '0', '.css');
-                        expect(runtime.content).to.contain(asyncJs.hash);
-                        expect(runtime.content).to.contain(asyncCss.hash);
-                    }));
+                    it('Works with mini-css-extract-plugin', () =>
+                        webpackCompile('mini-css-chunks', mode, setFutureEmitAssets)
+                            .then(sanityCheck)
+                            .then(stats => {
+                                const runtime = asset(stats, 'runtime~main');
+                                const asyncJs = asset(stats, '0', '.js');
+                                const asyncCss = asset(stats, '0', '.css');
+                                expect(runtime.content).to.contain(asyncJs.hash);
+                                expect(runtime.content).to.contain(asyncCss.hash);
+                            }));
 
-            it('Shows a warning if it is not the first plugin in the emit phase', () => {
-                sinon.stub(console, 'warn');
-                return webpackCompile('html-wrong-order', mode).then(stats => {
-                    expect(console.warn.called).to.be.true;
+                    it('Shows a warning if it is not the first plugin in the emit phase', () => {
+                        sinon.stub(console, 'warn');
+                        return webpackCompile('html-wrong-order', mode, setFutureEmitAssets).then(
+                            stats => {
+                                expect(console.warn.called).to.be.true;
+                            }
+                        );
+                    });
                 });
             });
         });
